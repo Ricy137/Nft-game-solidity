d74805728d26b92020258e0e7f09e2bb0b867d43
Ricy137 <1932353948@qq.com>
2022-08-28 01:31:55 +0800

    pref: hide contract address

--- scripts/run.ts
+++ scripts/run.ts
@@ -1,9 +1,10 @@
+import * as dotenv from "dotenv";
 import { ethers } from "hardhat";
 import { abi } from "../artifacts/contracts/MyEpicGame.sol/MyEpicGame.json";
 
 const attackBossTxn = async (): Promise<void> => {
   const connectedContract = new ethers.Contract(
-    "0x22B9a033BDe86dAf280Fc6b01530deF6d65F72c5",
+    `${process.env.CONTRACT_ADDRESS}`,
     abi
   );
 };
